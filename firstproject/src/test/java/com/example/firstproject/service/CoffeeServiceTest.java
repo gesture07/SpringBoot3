d04091542e796e81313e1296255b44e36f3dbe3c
Yoon DaHye <1130emma@naver.com>
2024-01-19 16:57:23 +0900

    day 13

---
+++ firstproject/src/test/java/com/example/firstproject/service/CoffeeServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeServiceTest {
+    @Autowired
+    ArticleService articleService;
+
+
+
+
+}
